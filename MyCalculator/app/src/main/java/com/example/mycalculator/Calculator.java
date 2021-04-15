38cb33c9642a20549ce0731e9df95cad7a77d1b5
KiborgBipBop <43749616+KiborgBipBop@users.noreply.github.com>
2021-04-15 03:15:04 +0300

    Added view Id's and "On Click" methods

--- MyCalculator/app/src/main/java/com/example/mycalculator/Calculator.java
+++ MyCalculator/app/src/main/java/com/example/mycalculator/Calculator.java
@@ -3,6 +3,7 @@ package com.example.mycalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Calculator extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class Calculator extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+    }
+
+    public void onNumberClick(View view) {
+    }
+
+    public void onOperationClick(View view) {
     }
 }
